0f1b30b74f9d26e0e6dccef1afde1a4f417e6ed3
gvasir <160545034+gvasir@users.noreply.github.com>
2025-10-14 13:23:19 -0400

    Create test_with_cals.py

---
+++ test_with_cals.py
@@ -0,0 +1,62 @@
+from collections import defaultdict
+import pytest
+
+def create_database():
+    return defaultdict(lambda: defaultdict(list))
+    
+
+
+def add_meal(database, user, date, meal):
+    database[user][date].append(meal)
+
+def get_meals(database, user, date):
+    return database[user][date]
+
+def add_calories(database, user, date, cals):
+    database[user][date].append(cals)
+
+def get_calories(database, user, date):
+    return database[user][date]
+
+
+
+# Tests below for Meal History ticket (saving user's previous meals and their cals)
+def test_initial_empty_database():
+    db = create_database()
+    assert isinstance(db, defaultdict)
+    assert db == {}
+
+def test_add_single_meal():
+    db = create_database()
+    add_meal(db, "User 1", "10-09-2026", "Chicken")
+    assert get_meals(db, "User 1", "10-09-2026") == ["Chicken"]
+
+def test_add_calories_to_meal():
+    db = create_database()
+    add_meal(db, "User 1", "10-09-2026", "Chicken")
+    add_calories(db, "User 1", "10-09-2026", "250")
+    assert get_meals(db, "User 1", "10-09-2026") == ["Chicken", "250"]
+
+def test_add_calories_to_meal_seprate_days():
+    db = create_database()
+    add_meal(db, "User 1", "10-09-2026", "Chicken")
+    add_calories(db, "User 1", "10-09-2026", "250")
+    add_meal(db, "User 1", "10-10-2026", "rice")
+    add_calories(db, "User 1", "10-10-2026", "100")
+    assert get_meals(db, "User 1", "10-09-2026") == ["Chicken", "250"]
+    assert get_meals(db, "User 1", "10-10-2026") == ["rice", "100"]
+
+
+def test_multiple_users_isolated():
+    db = create_database()
+    add_meal(db, "User 1", "10-09-2026", "Chicken")
+    add_meal(db, "User 2", "10-09-2026", "Steak")
+    add_calories(db, "User 1", "10-09-2026", "250")
+    add_calories(db, "User 2", "10-09-2026", "300")
+    assert get_meals(db, "User 1", "10-09-2026") == ["Chicken", "250"]
+    assert get_meals(db, "User 2", "10-09-2026") == ["Steak", "300"]
+    assert "User 3" not in db
+
+def test_empty_date_returns_empty_list():
+    db = create_database()
+    assert get_meals(db, "User 1", "12-31-2026") == []
