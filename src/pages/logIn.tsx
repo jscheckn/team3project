3268a166af0a376229173d1e3de4d8c4c8be57ff
gvasir <simv2022@gmail.com>
2025-11-25 01:09:03 -0500

    Login CSS

--- src/pages/logIn.tsx
+++ src/pages/logIn.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import "../Login.css";
 
 // https://www.geeksforgeeks.org/reactjs/react-hook-form-create-basic-reactjs-registration-and-login-form/ 
 
@@ -18,29 +19,33 @@ function Login() {
 
     return (
         <>
-            <h2>Login</h2>
+        <article>
+            <h2 id="title">Login</h2>
 
             <form className="login" onSubmit={handleSubmit(onSubmit)}>
                 <input
                     type="email"
                     {...register("email", { required: true })}
                     placeholder="Email"
+                    id="email"
                 />
                 {/* {errors.email && <span style={{ color: "red" }}>*Email* is mandatory</span>} */}
-
+                <br></br>
                 
                 <input
                     type="password"
                     {...register("password", { required: true })}
                     placeholder="Password"
+                    id="password"
                 />
                 {/* {errors.email && <span style={{ color: "red" }}>*Email* is mandatory</span>} */}
-
-
-                <input type="submit"  />
+                <br></br>
+                <input type="submit" className="button-54"  />
             </form>
-            <h3>Not Signed up?</h3>
-            <li><Link to="/signUp">Create On Here</Link></li>
+            <h3 id="NotSigned">Not Signed up?</h3>
+            <h4 id="SignUp"><Link to="/signUp" >Create On Here</Link></h4>
+        </article>
+            
         </>
     );
 }
