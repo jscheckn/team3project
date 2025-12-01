aac22d8f549d02fddf5f91a1b0fcf2b919bb0caf
gvasir <simv2022@gmail.com>
2025-12-01 10:50:40 -0500

    more CSS

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,21 +1,21 @@
     // Navbar.js
     import React from 'react';
     import { Link } from 'react-router-dom';
+    import "../CSS/Nav.css"
 
     function NavBar() {
       return (
         <nav className="navbar">
           <div className="navbar-brand">
-            <Link to="/">My Progress</Link> {/* Link to home page */}
+            <button className="MyProg"><Link id="MyProg" to="/">My Progress</Link></button> {/* Link to home page */}
+            <button className="login-links"><Link to="/logIn">Login</Link></button>
           </div>
           <ul className="navbar-links">
-            <li><Link to="/addMeal">Add Meal</Link></li>
-            <li><Link to="/addGoal">Add Goal</Link></li>
+            <li><button className="navbar-links-Meal"><Link to="/addMeal">Add Meal</Link></button></li>
+            <li><button className="navbar-links-Goal"><Link to="/addGoal">Add Goal</Link></button></li>
           </ul>
-          <ul className="login-links">
-            <li><Link to="/logIn">Login</Link></li>
-          </ul>
-          <hr></hr>
+
+          <hr id="LINE"></hr>
         </nav>
       );
     };
