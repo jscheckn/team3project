1b7a011331cf12ab98976c8ba59f52b31494447e
Thomas Kain <tkain@stevens.edu>
2025-12-01 22:14:23 -0500

    Display navbar items based on authentication state
    This also adds a signup item for unauthenticated users and a logout item for authenticated users (currently unimplemented).

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,22 +1,44 @@
     // Navbar.js
     import React from 'react';
     import { Link } from 'react-router-dom';
+    import FetchingFragment from './FetchingFragment';
     import "../CSS/Nav.css"
 
     function NavBar() {
-      return (
-        <nav className="navbar">
-          <div className="navbar-brand">
-            <button className="MyProg"><Link id="MyProg" to="/">My Progress</Link></button> {/* Link to home page */}
-            <button className="login-links"><Link to="/logIn">Login</Link></button>
-          </div>
-          <ul className="navbar-links">
-            <li><button className="navbar-links-Meal"><Link to="/addMeal">Add Meal</Link></button></li>
-            <li><button className="navbar-links-Goal"><Link to="/addGoal">Add Goal</Link></button></li>
-          </ul>
-
-          <hr id="LINE"></hr>
-        </nav>
+      return FetchingFragment(
+        '/api/accounts/validate',
+        <div>Loading...</div>,
+        error => <div style={{ color: 'red' }}>Error: {error}</div>,
+        authResult => {
+          let navItems = <>
+            <div className="navbar-brand">
+              <button className="login-links"><Link to="/logIn">Login</Link></button>
+              <button className="login-links"><Link to="/signUp">Sign Up</Link></button>
+            </div>
+          </>;
+          if (authResult.error === undefined)
+            navItems = <>
+              <div className="navbar-brand">
+                <button className="MyProg"><Link id="MyProg" to="/">My Progress</Link></button> {/* Link to home page */}
+                <button className="login-links"><Link to="/logOut">Logout</Link></button>
+              </div>
+              <ul className="navbar-links">
+                <li><button className="navbar-links-Meal"><Link to="/addMeal">Add Meal</Link></button></li>
+                <li><button className="navbar-links-Goal"><Link to="/addGoal">Add Goal</Link></button></li>
+              </ul>
+            </>;
+          return (
+            <nav className="navbar">
+              {navItems}
+              <hr id="LINE"></hr>
+            </nav>
+          )
+        },
+        {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({token: document.cookie})
+        }
       );
     };
 
